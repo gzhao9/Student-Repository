ed8264f99aae5cda50bacd8ec0112d888ae8ba7e
Gzhao <>
2020-11-19 03:59:33 +0500

    HW10

--- HW10/HW10_Gengwu_Zhao.py
+++ HW10/HW10_Gengwu_Zhao.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta, date
 from typing import Tuple, Iterator,List
 from prettytable import PrettyTable
 from collections import defaultdict
+#from HW08_Gengwu_Zhao import file_reader
 import os,sys
 def file_reader(path:str, fields: int, sep: str =',', header: bool=False) -> Iterator[List[str]]:
     try:
@@ -31,11 +32,32 @@ def file_reader(path:str, fields: int, sep: str =',', header: bool=False) -> Ite
         else:
             break  
 class Student:
-    def __init__(self,cwid:str,name:str,major:str):
+    def __init__(self,cwid:str,name:str,major:str,remind_course=list()):
         self.cwid = cwid
         self.name = name
         self.major = major
+        self.complete_course=list()
+        self.remind_course=dict()
+        self.gpa=0
         self.course = defaultdict(str)
+        self.change_remind_course(remind_course)
+    
+    def change_remind_course(self,require_course:list()):
+        for i in require_course:
+            self.remind_course[i.course]=i
+
+    def get_course_info(self):
+        grade_value={'A':4.0,'A-':3.75,'B+':3.25,'B':3.0,'B-':2.75,'C+':2.25,'C':2.0,'C-':0,'D+':0,'D':0,'D-':0,'F':0}
+        sums=0
+        for i,j in self.course.items():
+            sums+=grade_value[j]
+            if grade_value[j]!=0:
+                self.complete_course.append(i)
+                if i in self.remind_course.keys():
+                    del self.remind_course[i]
+        if len(self.course.values())!=0:
+            self.gpa=sums/len(self.course.values())
+    
     def __str__(self):
         pri=''
         for i,j in self.course.items():
@@ -54,51 +76,96 @@ class Instructors:
             pri+=i+':'+j+'\n'
         return f"CWID:{self.cwid} name: {self.name}\n Students number of the cousre:\n {pri} "
 
+class Course:
+    def __init__(self,Major:str,Required:str,course:str):
+        self.mjao=Major
+        self.required=Required
+        self.course=course
+    def __str__(self):
+        return f"Course: {self.course} Major: {self.mjao} Required: {self.required} "
+
 class Repository:
     def __init__(self,path):
         os.chdir(path)
         self.path=path
         self.students=dict()
         self.instructors=dict()
+        self.major=defaultdict(list)
         self.get_info()
     #get the info form the path
     def get_info(self):
-        #get teh students info
-        for line in file_reader(self.path+'students.txt',3,'\t'):
+
+        #get the Major info
+        for line in file_reader(self.path+'majors.txt',3,'\t',True):
+            self.major[line[0]].append(Course(line[0],line[1],line[2]))
+
+        #get the students info
+        for line in file_reader(self.path+'students.txt',3,';',True):
             if line[0] in self.students.keys():
                 raise ValueError(f'The info of this students {line[0]} has been added')
-            self.students[line[0]]=Student(line[0],line[1],line[2])
+            self.students[line[0]]=Student(line[0],line[1],line[2],self.major[line[2]])
+        
         #get the instructors info
-        for line in file_reader(self.path+'instructors.txt',3,'\t'):
+        for line in file_reader(self.path+'instructors.txt',3,'|',True):
             if line[0] in self.instructors.keys():
                 raise ValueError(f'The info of this instructors {line[0]} has been added')
             self.instructors[line[0]]=Instructors(line[0],line[1],line[2])
+        
+       
         #get the grade info
-        for line in file_reader(self.path+'grades.txt',4,'\t'):
+        for line in file_reader(self.path+'grades.txt',4,'|',True):
             if self.students[line[0]].course[line[1]] is not None:
                 self.students[line[0]].course[line[1]]=line[2]
                 self.instructors[line[3]].course[line[1]]+=1
             else:
                 raise ValueError(f'The grade of this student {line[0]} has been added')
-    
+        
+        #Calculate student course information
+        for i in self.students.keys():
+            self.students[i].get_course_info()
+
     def print_students_table(self):
         row:PrettyTable=PrettyTable()
-        row.field_names=['cwid','name','major','course']
+        row.field_names=['Cwid','Name','Major','Complete Courses','Remaining Requireed','Remaining Electives','Gpa']
         for i,l in self.students.items():
-            row.add_row([i,l.name,l.major,','.join(sorted(list(l.course.keys())))])
+            rc=list()
+            ec=list()
+            for x in l.remind_course.values():
+                if x.required=='R':
+                    rc.append(x.course)
+                else:
+                    ec.append(x.course)
+            row.add_row([i,l.name,l.major,sorted(l.complete_course),sorted(rc),sorted(ec),round(l.gpa, 2)])
         print('Student Summary')
+        print(row)
+    
+    def print_major_table(self):
+        row:PrettyTable=PrettyTable()
+        row.field_names=['Major','Require Course','Electives']
+        for i,l in self.major.items():
+            rc=list()
+            ec=list()
+            for x in l:
+                if x.required=='R':
+                    rc.append(x.course)
+                else:
+                    ec.append(x.course)
+            row.add_row([i,sorted(rc),sorted(ec)])
+        print('Major Summary')
         print(row)
 
     def print_instructors_table(self):
         row1:PrettyTable=PrettyTable()
-        row1.field_names=['cwid','name','department','course', 'students']
+        row1.field_names=['Cwid','Name','Department','Course', 'Students']
         for i,l in self.instructors.items():
             for x,y in l.course.items():
                 row1.add_row([i,l.name,l.department,x,y])
         print('Instructor Summary')
         print(row1)
+
 if __name__ == "__main__":
-    t=Repository(os.getcwd()+"\\week09\\")
+    t=Repository(os.getcwd()+"\\week10\\")
     print(t.students['11461'])
+    t.print_major_table()
     t.print_students_table()
     t.print_instructors_table()
--- HW10/HW10_Test_Gengwu_Zhao.py
+++ HW10/HW10_Test_Gengwu_Zhao.py
@@ -1,5 +1,5 @@
 import unittest,os
-from HW09_Gengwu_Zhao import Repository
+from HW10_Gengwu_Zhao import Repository
 class TestRepository(unittest.TestCase):
     def test_Repository(self):
         t=Repository(os.getcwd()+"\\week10\\")
@@ -11,6 +11,8 @@ class TestRepository(unittest.TestCase):
         self.assertEqual(t.students['11788'].name,'Fuller, E')
         self.assertEqual(t.students['11788'].major,'SYEN')
         self.assertEqual(t.students['11461'].course['SYS 800'],'A')
-        self.assertEqual(t.students['11461'].course['SYS 100000'],'')        
+        self.assertEqual(t.students['11461'].course['SYS 100000'],'')
+        self.assertEqual(round(t.students['11461'].gpa,2),3.92)
+        
 if __name__ == '__main__':
     unittest.main(exit=False, verbosity=2)
